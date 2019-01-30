834deb08f6dfd8a777feba343829efab8abf0dcf
lin7 <908069808@qq.com>
2019-01-30 17:48:26 +0800

    stage 2

--- AccountOE/app/src/main/java/com/example/lin9080/accountoe/MainActivity.java
+++ AccountOE/app/src/main/java/com/example/lin9080/accountoe/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.lin9080.accountoe;
 
+import android.content.DialogInterface;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,6 +28,7 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private int conut=1;
+    private DrawerLayout mDrawerLayout;
     private ArrayList<Account> accounts=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         android.support.v7.widget.Toolbar toolbar =findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.toleft_dr);
+        }
         ((Button)findViewById(R.id.createALitePal)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,10 +84,27 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.clear:
-                DataSupport.deleteAll(Account.class);
+                AlertDialog.Builder dialog=new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("清空记录").setMessage("是否清空所有用户账目?").setCancelable(true);
+                dialog.setPositiveButton("清除", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        DataSupport.deleteAll(Account.class);
+                    }
+                });
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                dialog.show();
                 break;
             case R.id.change:
                 Toast.makeText(MainActivity.this,"编辑功能可用",Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
                 break;
             default:
         }
